45d0991da8ef6af38e9b54e42d8f234e55a2c398
Timothy <timothyrwashburn@gmail.com>
2024-11-09 10:22:49 -0800

    initial commit

---
+++ Pat/PatApp.swift
@@ -0,0 +1,17 @@
+//
+//  PatApp.swift
+//  Pat
+//
+//  Created by Timothy Washburn on 11/9/24.
+//
+
+import SwiftUI
+
+@main
+struct PatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PatTests/PatTests.swift
@@ -0,0 +1,17 @@
+//
+//  PatTests.swift
+//  PatTests
+//
+//  Created by Timothy Washburn on 11/9/24.
+//
+
+import Testing
+@testable import Pat
+
+struct PatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
