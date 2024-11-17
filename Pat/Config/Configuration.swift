d4e710323c251c3a67ccde4e073ad7a00299047f
Timothy <timothyrwashburn@gmail.com>
2024-11-17 12:27:24 -0800

    prepend https://

--- Pat/Config/Configuration.swift
+++ Pat/Config/Configuration.swift
@@ -3,15 +3,15 @@ import Foundation
 enum PatConfig {
     static let apiURL: String = {
         #if DEBUG
-        guard let url = Bundle.main.object(forInfoDictionaryKey: "API_URL") as? String else {
+        guard let configURL = Bundle.main.object(forInfoDictionaryKey: "API_URL") as? String else {
             fatalError("API_URL not found in Development.xcconfig")
         }
-        return url
+        return "https://" + configURL
         #else
-        guard let url = Bundle.main.object(forInfoDictionaryKey: "API_URL") as? String else {
+        guard let configURL = Bundle.main.object(forInfoDictionaryKey: "API_URL") as? String else {
             fatalError("API_URL not found in Production.xcconfig")
         }
-        return url
+        return "https://" + configURL
         #endif
     }()
 }
