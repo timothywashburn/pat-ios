0fb6e65604baf88c0113ae4ef157f16a110b1ffa
Timothy <timothyrwashburn@gmail.com>
2024-11-17 12:15:59 -0800

    simplify configuration

--- Pat/Config/Configuration.swift
+++ Pat/Config/Configuration.swift
@@ -1,30 +1,17 @@
 import Foundation
 
-enum Configuration {
-    enum Error: Swift.Error {
-        case missingKey, invalidValue
-    }
-
-    static func value<T>(for key: String) throws -> T where T: LosslessStringConvertible {
-        guard let object = Bundle.main.object(forInfoDictionaryKey: key) else {
-            throw Error.missingKey
+enum PatConfig {
+    static let apiURL: String = {
+        #if DEBUG
+        guard let url = Bundle.main.object(forInfoDictionaryKey: "API_URL") as? String else {
+            fatalError("API_URL not found in Development.xcconfig")
         }
-
-        switch object {
-        case let value as T:
-            return value
-        case let string as String:
-            guard let value = T(string) else { fallthrough }
-            return value
-        default:
-            throw Error.invalidValue
+        return url
+        #else
+        guard let url = Bundle.main.object(forInfoDictionaryKey: "API_URL") as? String else {
+            fatalError("API_URL not found in Production.xcconfig")
         }
-    }
-}
-
-struct PatConfig {
-    static var apiURL: String {
-        let baseURL = (try? Configuration.value(for: "API_BASE_URL") as String) ?? "pat.timothyw.dev"
-        return "https://\(baseURL)"
-    }
+        return url
+        #endif
+    }()
 }
